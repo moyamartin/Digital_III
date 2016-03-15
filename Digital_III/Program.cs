63cb7ae04b6bc2d7728b281a2498cc3eadb9fbb1
Martín <moyamartin1@gmail.com>
2016-03-15 01:01:41 -0300

    Added Serial Port library

--- Digital_III/Program.cs
+++ Digital_III/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using Gtk;
+using System.IO.Ports;
 
 namespace Digital_III
-{
+{	
 	class MainClass
 	{
+		public static SerialPort _serialPort;
 		public static void Main (string[] args)
 		{
 			Application.Init ();
 			MainWindow win = new MainWindow ();
 			win.Show ();
 			Application.Run ();
+
+			_serialPort = new SerialPort ();
 		}
 	}
 }
